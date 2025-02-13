3fcd8818ad04b6a31637051cf3a0f4ba8dbe28bd
BrittanyKCohen96 <brittanykcohen96@gmail.com>
2025-02-13 10:47:55 +0200

    Final changes to stanley

--- mobility_ctrl/src/pure_pursuit/setup.py
+++ mobility_ctrl/src/pure_pursuit/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'pure_pursuit'
 
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # Include config files if you have any
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Include urdf files if you have any
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro.urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
