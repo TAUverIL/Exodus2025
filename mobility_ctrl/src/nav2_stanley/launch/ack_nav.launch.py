61ac577d293b54afa12fe659aa4db58933093ec4
BrittanyKCohen96 <brittanykcohen96@gmail.com>
2025-05-23 16:07:48 +0300

    Added navigation YAML for diff drive option

--- mobility_ctrl/src/nav2_stanley/launch/ack_nav.launch.py
+++ mobility_ctrl/src/nav2_stanley/launch/ack_nav.launch.py
@@ -7,24 +7,36 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.conditions import IfCondition, UnlessCondition
 
 
 def generate_launch_description():
 
     pkg_share = get_package_share_directory('nav2_stanley')
+    controller = LaunchConfiguration('controller')
 
     # Update navigation parameters (find yaml file) and map file path
-    params_file = os.path.join(pkg_share, 'config/navigation.yaml')
+    ack_params_file = os.path.join(pkg_share, 'config/ack_navigation.yaml')
+    diff_params_file = os.path.join(pkg_share, 'config/diff_navigation.yaml')
     map_file = os.path.join(pkg_share, 'maps/ack_map.yaml')
 
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
 
     # Start navigation
-    nav2_bringup_launch = IncludeLaunchDescription(
+    ack_nav2_bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_nav2_bringup, 'launch/navigation_launch.py')),
-        launch_arguments={'use_sim_time': 'True', 'params_file': params_file}.items(),
+        launch_arguments={'use_sim_time': 'True', 'params_file': ack_params_file}.items(),
+        condition=IfCondition(PythonExpression(["'", controller, "' == 'ackermann'"]))
+    )
+
+    diff_nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_nav2_bringup, 'launch/navigation_launch.py')),
+        launch_arguments={'use_sim_time': 'True', 'params_file': ack_params_file}.items(),
+        condition=IfCondition(PythonExpression(["'", controller, "' == 'diff_drive'"]))
     )
 
     map_server_node = Node(
@@ -47,7 +59,9 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            nav2_bringup_launch,
+            DeclareLaunchArgument(name='controller', default_value='ackermann', description='Choose controller to use'),
+            ack_nav2_bringup_launch,
+            diff_nav2_bringup_launch,
             map_server_node,
             map_server_lifecycle_node
         ]
